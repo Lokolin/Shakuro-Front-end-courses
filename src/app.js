c68871d09b70a2258b765d809d0701e5d5bddbc3
Lokolin <lokolin777@mail.ru>
2019-06-28 20:57:13 +0300

    Add responsive design  to Blog

--- src/app.js
+++ src/app.js
@@ -1,4 +1,9 @@
-import './scss/styles.scss'
-import './scss/home.scss'
-import './scss/normalize.scss'
-import './scss/blog.scss'
+import './scss/styles.scss';
+import './scss/home.scss';
+import './scss/normalize.scss';
+import './scss/blog.scss';
+import './js/navbar.js';
+import '@fortawesome/fontawesome-free/js/fontawesome';
+import '@fortawesome/fontawesome-free/js/solid';
+import '@fortawesome/fontawesome-free/js/regular';
+import '@fortawesome/fontawesome-free/js/brands';
--- webpack.config.js
+++ webpack.config.js
@@ -1,48 +1,78 @@
 const path = require('path');
-const ExtractTextPlugin = require("extract-text-webpack-plugin");
-const HtmlWebpackPlugin = require('html-webpack-plugin'); 
-const CopyWebpackPlugin= require('copy-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const webpack = require('webpack');
 
 module.exports = {
   entry: './src/app.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js'
+    filename: 'bundle.js',
   },
   module: {
+    // loaders: [
+    //   // the url-loader uses DataUrls.
+    //   // the file-loader emits files.
+    //   {
+    //     test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+    //     loader: 'url-loader?limit=10000&mimetype=application/font-woff',
+    //   },
+    //   {
+    //     test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+    //     loader: 'file-loader',
+    //   },
+    // ],
     rules: [
       {
+        test: /.(ttf|otf|eot|svg|woff(2)?)(\?[a-z0-9]+)?$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: '../fonts/',
+              publicPath: '../static/fonts',
+            },
+          },
+        ],
         test: /\.scss$/,
         use: ExtractTextPlugin.extract({
           fallback: 'style-loader',
-          use: ['css-loader', 'sass-loader']
-        })
-      }
+          use: ['css-loader', 'sass-loader'],
+        }),
+      },
     ],
   },
   plugins: [
+    new webpack.ProvidePlugin({
+      $: 'jquery',
+      jQuery: 'jquery',
+    }),
     new ExtractTextPlugin('style.css'),
     new HtmlWebpackPlugin({
-      template: __dirname + "/index.html",
+      template: __dirname + '/index.html',
       inject: 'body',
     }),
-    new HtmlWebpackPlugin({  // Also generate a test.html
+    new HtmlWebpackPlugin({
+      // Also generate a test.html
       filename: 'blog.html',
-      template: 'blog.html'
+      template: 'blog.html',
     }),
-    new CopyWebpackPlugin([{
-      from: './src/fonts',
-      to: './fonts'
-    },
-    {
-      from: './src/img',
-      to: './img'
-    }
-  ])
-
+    new CopyWebpackPlugin([
+      {
+        from: './src/fonts',
+        to: './fonts',
+      },
+      {
+        from: './src/img',
+        to: './img',
+      },
+    ]),
   ],
-  devServer: {  // configuration for webpack-dev-server
-      contentBase: './src/public',  //source of static assets
-      port: 7700, // port to run dev-server
-  } 
+  devServer: {
+    // configuration for webpack-dev-server
+    contentBase: './src/public', //source of static assets
+    port: 7700, // port to run dev-server
+  },
 };
