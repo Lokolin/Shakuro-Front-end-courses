c0a23f2f796dc6dced32797d82ab8adffae93af4
Lokolin <lokolin777@mail.ru>
2019-07-22 22:00:59 +0300

    Fix mistakes

--- src/js/store.js
+++ src/js/store.js
@@ -1,55 +1,104 @@
-let currentPage = 1;
-let totalPages;
-const numOfItems = 6;
+(function() {
+  let currentPage = 1;
+  let totalPages;
+  const numOfItems = 6;
 
-window.onload = function() {
-  var template = document.getElementById('template-list-item');
-  var templateHtml = template.innerHTML;
-  var listHtml = '';
+  let template = document.getElementById('template-list-item');
+  let templateHtml = template.innerHTML;
+  let listHtml = '';
 
   fetch('/data/page1.json')
     .then(function(resp) {
       return resp.json();
     })
     .then(function(data) {
-      for (let key = 0; key < 6; key++) {
-        listHtml += templateHtml
-          .replace(/{{subtitle}}/g, data['items'][key]['subtitle'])
-          .replace(/{{image}}/g, data['items'][key]['image'])
-          .replace(/{{name}}/g, data['items'][key]['name'])
-          .replace(/{{price}}/g, data['items'][key]['price']);
-      }
+      listHtml = fillTemplate(data);
       document.getElementById('store-items').innerHTML = listHtml;
       totalPages = data.meta.totalPages;
+      onClickAction();
     });
 
   window.addEventListener('scroll', function() {
     let additionalHtml = '';
-    var block = document.getElementById('store-items');
-    var contentHeight = block.offsetHeight;
-    var yOffset = window.pageYOffset;
-    var window_height = window.innerHeight;
-    var y = yOffset + window_height;
+    let block = document.getElementById('store-items');
+    let contentHeight = block.offsetHeight;
+    let yOffset = window.pageYOffset;
+    let windowHeight = window.innerHeight;
+    let y = yOffset + windowHeight;
 
     if (y >= contentHeight) {
       currentPage = currentPage + 1;
+
       if (currentPage <= totalPages) {
         fetch('/data/page' + currentPage + '.json')
           .then(function(resp) {
             return resp.json();
           })
           .then(function(data) {
-            for (let key = 0; key < 6; key++) {
-              additionalHtml += templateHtml
-                .replace(/{{subtitle}}/g, data['items'][key]['subtitle'])
-                .replace(/{{image}}/g, data['items'][key]['image'])
-                .replace(/{{name}}/g, data['items'][key]['name'])
-                .replace(/{{price}}/g, data['items'][key]['price']);
-            }
+            additionalHtml = '';
+            additionalHtml = fillTemplate(data);
             document.getElementById('store-items').innerHTML += additionalHtml;
             totalPages = data.meta.totalPages;
+            onClickAction();
           });
       }
     }
   });
-};
+
+  function fillTemplate(data) {
+    let newTemplate = '';
+    for (let key = 0; key < numOfItems; key++) {
+      newTemplate += templateHtml
+        .replace(/{{subtitle}}/g, data.items[key]['subtitle'])
+        .replace(/{{image}}/g, data.items[key]['image'])
+        .replace(/{{name}}/g, data.items[key]['name'])
+        .replace(/{{price}}/g, data.items[key]['price']);
+    }
+    return newTemplate;
+  }
+
+  document.getElementById('myInput').onkeyup = function storeSearch() {
+    var input, filter, itemsBlock, item, name, i;
+    input = document.getElementById('myInput');
+    filter = input.value.toUpperCase();
+    itemsBlock = document.getElementById('store-items');
+    item = itemsBlock.getElementsByClassName('blog-article-card');
+
+    for (i = 0; i < item.length; i++) {
+      name = item[i].getElementsByTagName('p')[0];
+      if (name.innerHTML.toUpperCase().indexOf(filter) > -1) {
+        item[i].style.display = '';
+      } else {
+        item[i].style.display = 'none';
+      }
+    }
+  };
+
+  function getCardInfo(name) {
+    itemSubtitle = name.querySelector('#subtitle').innerHTML;
+    itemName = name.querySelector('.store-text').innerHTML;
+    itemPrice = name.querySelector('#price').innerHTML;
+    console.log(
+      'Category: ' +
+        itemSubtitle +
+        '\n' +
+        'Item name: ' +
+        itemName +
+        '\n' +
+        'Item price:' +
+        itemPrice,
+    );
+  }
+
+  let onClickAction = () => {
+    let links = document.getElementsByClassName('cardInfoLink');
+    Array.from(links).forEach(element => {
+      element.onclick = () => getCardInfo(element);
+    });
+  };
+
+  window.onload = function() {
+    let spinner = document.querySelector('#loading');
+    spinner.style.visibility = 'hidden';
+  };
+})();
